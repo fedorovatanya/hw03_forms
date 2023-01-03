2884b0e6bd2e845c6c834e5c7f1f1c8d1570abb4
Tanya Fedorova <fedorovatd@gmail.com>
2023-01-03 18:52:56 +0300

    save

---
+++ yatube/posts/views.py
@@ -0,0 +1,86 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group, User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from . forms import PostForm
+
+
+def index(request):
+    title = "Последние обновления на сайте"
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'title': title,
+        'posts': posts,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_list(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'group': group,
+               'posts': posts,
+               'page_obj': page_obj}
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = author.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            temp_form = form.save(commit=False)
+            temp_form.author = request.user
+            temp_form.save()
+            return redirect(
+                'posts:profile', temp_form.author
+        )
+    else:
+        form = PostForm()
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect(
+            'posts:post_detail', post_id
+        )
+    form = PostForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect(
+            'posts:post_detail', post_id
+        )
+    return render(request, 'posts/create_post.html', {
+        'form': form, 'is_edit': True, 'post': post
+    })
