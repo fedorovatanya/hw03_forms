3389072a92786499612e6817f0b843dec4f918aa
Tanya Fedorova <fedorovatd@gmail.com>
2023-01-16 21:12:24 +0300

    fix

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -69,7 +69,7 @@ def post_create(request):
         {'form': form, "groups": groups}
     )
 
-
+    
 @login_required
 def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
