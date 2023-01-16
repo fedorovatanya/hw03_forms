bd60d5cafb18e560329a928f6b3beb67ad216ddf
Tanya Fedorova <fedorovatd@gmail.com>
2023-01-16 21:06:10 +0300

    fix views templares

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -56,39 +56,37 @@ def post_detail(request, post_id):
 
 @login_required
 def post_create(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect(f"/profile/{post.author}/", {"form": form})
-    form = PostForm()
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', post.author)
     groups = Group.objects.all()
-    template = "posts/create_post.html"
-    context = {"form": form, "groups": groups}
-    return render(request, template, context)
+    return render(
+        request,
+        "posts/create_post.html",
+        {'form': form, "groups": groups}
+    )
 
 
+@login_required
 def post_edit(request, post_id):
-    is_edit = True
     post = get_object_or_404(Post, pk=post_id)
-    author = post.author
     groups = Group.objects.all()
     form = PostForm(request.POST or None, instance=post)
-    template = "posts/create_post.html"
-    if request.user == author:
-        if request.method == "POST" and form.is_valid:
-            post = form.save()
-            return redirect("posts:post_detail", post_id)
-        context = {
-            "form": form,
-            "is_edit": is_edit,
-            "post": post,
-            "groups": groups,
-        }
-        return render(request, template, context)
-    return redirect("posts:post_detail", post_id)
+    context = {
+        "form": form,
+        "is_edit": True,
+        "post": post,
+        "groups": groups,
+    }
+    if post.author != request.user:
+        return redirect("posts:post_detail", post_id)
+    if form.is_valid():
+        form.save()
+        return redirect("posts:post_detail", post_id)
+    return render(request, "posts/create_post.html", context)
 
 
 def get_page_paginator(request, posts):
